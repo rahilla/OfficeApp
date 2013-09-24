48c1b4c5a49d18f4667f297d5d40cad0e92cfd9e
Rahilla Zafar <rahillazafar@Rahillas-MacBook-Air.local>
2013-09-24 12:36:07 -0400

    init

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :venue, :address, :city, :country, :name, :url, :email, :phone
 
-        belongs_to :user
-        belongs_to :event
-
         has_and_belongs_to_many :events
         has_and_belongs_to_many :users
 
