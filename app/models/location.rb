4c3a930782cb28307715fef504fa161e1936e49d
Rahilla Zafar <rahillazafar@Rahillas-MacBook-Air.local>
2013-09-24 12:30:48 -0400

    init

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,7 @@ class Location < ActiveRecord::Base
         belongs_to :user
         belongs_to :event
 
-        has_and_belongs_to_many :events :users
+        has_and_belongs_to_many :events
+        has_and_belongs_to_many :users
 
 end
