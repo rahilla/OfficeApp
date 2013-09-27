939c8c98a9cecc1981f0b5f695a3f217df73d357
Rahilla Zafar <rahillazafar@Rahillas-MacBook-Air.local>
2013-09-27 08:51:38 -0400

    Heroku Launching

--- db/migrate/20130925010846_add_latitude_longitude_gmaps_to_location.rb
+++
@@ -1,7 +0,0 @@
-class AddLatitudeLongitudeGmapsToLocation < ActiveRecord::Migration
-  def change
-    add_column :users, :latitude,  :float #you can change the name, see wiki
-    add_column :users, :longitude, :float #you can change the name, see wiki
-    add_column :users, :gmaps, :boolean #not mandatory, see wiki
-  end
-end
